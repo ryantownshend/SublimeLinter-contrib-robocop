7458409e2141de2e15a8675c99ede86a74dac350
Ryan Townshend <rtownshend@spiria.com>
2022-06-13 16:59:59 -0300

    added basic regex test

--- linter.py
+++ linter.py
@@ -2,43 +2,17 @@
 from SublimeLinter.lint import PythonLinter
 
 
+# filename, line, col, warning, error, code, message
+PATTERN = r'[\w\/\-]*\/(?P<filename>\w+\.\w+)\:(?P<line>\d+)\:(?P<col>\d+) \[(?P<warning>W|I|)?(?P<error>E)?\] (?P<code>\d+) (?P<message>.*)'  # noqa
+
+
 class Robocop(PythonLinter):
     """Robocop linter."""
 
     cmd = 'robocop'
     # regex = r'(.*)\:(\d+)\:(\d+) \[(W|I|E)\] (\d+) (.*)'
-    regex = '(?P<filename>.*)\:(?P<line>\d+)\:(?P<col>\d+) \[(?P<warning>W|I|)?(?P<error>E)?\] (?P<code>\d+) (?P<message>.*)'
+    regex = PATTERN
     multiline = False
     defaults = {
         'selector': 'source.robot'
     }
-
-'''
-Name    | Description                                   | example
---------|-----------------------------------------------|-----------------------
-line    | The line number on which the problem occurred | 16
-col     | The column number where the error occurred    | 1
-message | The description of the problem                | Keyword name (to eol)
-error   | the error will be marked as an error by       | [E]
-warning | the error will be marked as a warning by      | [W] or [I]
-code    | The corresponding error code given            | 0302
-
-
-
-/Users/rtownshend/repos/timesheet-e2e/TestCases/gherkin_login.robot:16:1 [W] 0302 Keyword name 'When User 'rtownshend' Logs In With Password 'a'' does not follow case convention (wrong-case-in-keyword-name)
-
-/Users/rtownshend/repos/timesheet-e2e/TestCases/gherkin_login.robot:28:1 [I] 0302 Keyword name 'Input password' does not follow case convention (wrong-case-in-keyword-name)
-
-/Users/rtownshend/repos/timesheet-e2e/TestCases/timesheet_login.robot:11:1 [E] 0313 Test case name should not be empty (test-case-name-is-empty)
-
-# cmd = 'pyflakes'
-# regex = r'''(?x)
-#     ^(?P<filename>.+?):(?P<line>\d+):((?P<col>\d+):?)?\s
-#     # The rest of the line is the error message.
-#     # Within that, capture anything within single quotes as `near`.
-#     (?P<message>[^\'\n\r]*(?P<near>\'.+?\')?.*)
-# '''
-
-'''
-
-
---
+++ test_linter.py
@@ -0,0 +1,50 @@
+"""Test the linter.
+
+https://stackoverflow.com/questions/8658043/how-to-mock-an-import
+"""
+import re
+import json
+import unittest
+import sys
+from unittest.mock import Mock
+sys.modules["SublimeLinter.lint"] = Mock()
+import linter  # noqa
+
+
+def load_data():
+    """Load the test data."""
+    with open("linter_test_data.json", "r", encoding="utf-8") as reader:
+        return json.load(reader)
+
+
+class TestLinter(unittest.TestCase):
+    """Test the linter."""
+
+    def test_regex_pattern(self):
+        """Test the linter's regex pattern."""
+        data = load_data()
+        pattern = re.compile(linter.PATTERN)
+        result = pattern.match(data["error"])
+
+        # filename, line, col, warning, error, code, message
+
+        self.assertEqual(
+            result.group("line"),
+            "11"
+        )
+        self.assertEqual(
+            result.group("col"),
+            "1"
+        )
+        self.assertEqual(
+            result.group("error"),
+            "E"
+        )
+        self.assertEqual(
+            result.group("code"),
+            "0313"
+        )
+        self.assertEqual(
+            result.group("message"),
+            "Test case name should not be empty (test-case-name-is-empty)"
+        )
