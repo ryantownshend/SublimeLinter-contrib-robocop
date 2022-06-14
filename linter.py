33fe8baef4ddd42e9752238a3c5ba34e6d8921d0
Ryan Townshend <rtownshend@spiria.com>
2022-06-14 11:51:35 -0300

    now handles multiple files correctly

--- linter.py
+++ linter.py
@@ -2,16 +2,14 @@
 from SublimeLinter.lint import PythonLinter
 
 
-# filename, line, col, warning, error, code, message
-# PATTERN = r'[\w\/\-]*\/(?P<filename>\w+\.\w+)\:(?P<line>\d+)\:(?P<col>\d+) \[(?P<warning>W|I|)?(?P<error>E)?\] (?P<code>\d+) (?P<message>.*)'  # noqa
+# line, col, warning, error, code, message
 PATTERN = r"^.+?\:(?P<line>\d+)\:(?P<col>\d+) \[(?P<warning>W|I|)?(?P<error>E)?\] (?P<code>\d+) (?P<message>.*)"  # noqa
 
 
 class Robocop(PythonLinter):
     """Robocop linter."""
 
-    cmd = 'robocop'
-    # regex = r'(.*)\:(\d+)\:(\d+) \[(W|I|E)\] (\d+) (.*)'
+    cmd = ('robocop', '${file}')
     regex = PATTERN
     multiline = False
     defaults = {
